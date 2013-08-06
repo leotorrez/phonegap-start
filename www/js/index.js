bb14a2f16fe7a327db4a0c6025837c5b5daa169d
leotorrez <leotorrez@gmail.com>
2013-08-06 18:50:06 -0300

    update

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,25 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+$(function(){
+$( "#calc" ).on( "pagebeforeshow", function( event, ui ) {
+ahora = new Date();
+        ahora.setTime(System.currentTimeMillis());
+        fecha.setDate(ahora);
+        dia.setDate(ahora);
+    var fechanac =$('#fechanac').val();
+	var auxnac=fechanac.split("-"),
+	anonac=auxnac[0],
+	mesnac=auxnac[1],
+	dianac=auxnac[2];
+	var fechahoy =$('#fechahoy').val();
+	var auxhoy=fechahoy.split("-"),
+	anohoy=auxhoy[0],
+	meshoy=auxhoy[1],
+	diahoy=auxhoy[2];
+	
+  $('#biorritmocontenido').html(Date.parse($('#fechahoy').val()));
+});
+  $( "#calc" ).bind( "click", function(){
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
+  
+  
+  } );
+});
